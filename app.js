af696779e10491f9f32c09f2ec1b8f4b1138b4d7
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 00:29:51 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // write code here
+const array = ['Elephant', 'Ant', 'Cat', 'Eagle', 'Zebra'];
+const result = array.filter(item => item.startsWith('E'));
+console.log(result); // ['Elephant', 'Eagle']
+
+const array = ['APPLE', 'oRanGE', 'PEACH', 'PaPAYA'];
+const result = array.filter(item => item === item.toUpperCase());
+console.log(result); // ['APPLE', 'PEACH']
+
+
+const array = ['Krabi', 'Chonburi', 'Buriram', 'Saraburi', 'Phrae'];
+const result = array.filter(item => item.toLowerCase().includes('buri'));
+console.log(result); // ['Chonburi', 'Buriram', 'Saraburi']
